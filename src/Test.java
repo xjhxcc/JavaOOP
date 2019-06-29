d601a8f63df6effa24bc7112e76b527f2aeee349
xjhxcc <2351889342@qq.com>
2019-06-29 15:55:07 +0800

    第一次上传整个项目

---
+++ src/Test.java
@@ -0,0 +1,8 @@
+import model.Student;
+
+public class Test {
+    public static void main(String[] args) {
+        Student s=new Student();
+        System.out.println(s);
+    }
+}
---
+++ src/model/Student.java
@@ -0,0 +1,51 @@
+package model;
+
+public class Student {
+    //学生姓名，年龄，性别，学号
+    String name;
+    int age;
+    String sex;
+    int sno;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public int getSno() {
+        return sno;
+    }
+
+    public void setSno(int sno) {
+        this.sno = sno;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", sex='" + sex + '\'' +
+                ", sno=" + sno +
+                '}';
+    }
+}
